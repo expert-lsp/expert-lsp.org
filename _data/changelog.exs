42cf65821009aec9c7fbfcd702e4d456964a94ab
AltCode <altcode@vivaldi.net>
2026-08-09 01:14:11 +0200

    fix: fetch changelog from latest tag (#29)

--- _data/changelog.exs
+++ _data/changelog.exs
@@ -1,8 +1,16 @@
 if File.exists?("_build/changelog.md") do
   File.read!("_build/changelog.md")
 else
+  github_api = "https://api.github.com/repos/expert-lsp/expert/tags"
+  base_url = "https://raw.githubusercontent.com/expert-lsp/expert/refs"
+
   %{body: page} =
-    Req.get!("https://raw.githubusercontent.com/expert-lsp/expert/refs/heads/main/CHANGELOG.md")
+    with %{body: [object | _]} <- Req.get!(github_api),
+         %{"name" => tag} when not is_nil(tag) <- object do
+      Req.get!("#{base_url}/tags/#{tag}/CHANGELOG.md")
+    else
+      _ -> Req.get!("#{base_url}/heads/main/CHANGELOG.md")
+    end
 
   File.write!("_build/changelog.md", page)
 
